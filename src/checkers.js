0e06dd16973ac81c101a91a6d521a3272a8cedfa
tjc234 <tjc234@nau.edu>
2023-11-16 13:09:30 -0700

    outline checkers game with comments

---
+++ src/checkers.js
@@ -0,0 +1,31 @@
+// global constants for game board
+
+// initialize game board
+
+// declare array to store game board data
+
+// render game board
+
+// render game pieces
+
+// game logic
+
+    // moving pieces
+
+    // jumping pieces
+
+    // crowning pieces
+
+    // removing pieces
+
+    // win conditions
+
+    // manage turns
+  
+// manage the game board on page load
+
+    // event handlers
+
+    // render game board
+
+    // render game pieces
