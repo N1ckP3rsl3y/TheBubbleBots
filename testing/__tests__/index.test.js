7b107c1e92b9dfcac1a4eac4c4f3fa03fdb5693c
tjc234 <tjc234@nau.edu>
2023-11-19 15:00:58 -0700

    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- testing/__tests__/index.test.js
+++ testing/__tests__/index.test.js
@@ -1,24 +1,18 @@
+// testing/__tests__/index.test.js
 const { screen } = require('@testing-library/dom');
-
-// import your functions and variables from index.js
-// In your test file
 const { createBoard, renderBoard, makeListeners } = require('../../src/gameboard_dev/index.js');
-
 
 describe('Game Board Tests', () => {
   beforeEach(() => {
-    // set up a clean document body before each test
     document.body.innerHTML = '';
   });
 
   test('Create Board should render a board with pieces', () => {
     createBoard();
-    
-    // assert that the gameboard element is in the document
+
     const gameBoardElement = screen.getByTestId('gameboard');
     expect(gameBoardElement).toBeInTheDocument();
 
-    // assert that the gameboard contains child elements representing pieces
     const redPieces = screen.getAllByAltText('Red Piece');
     const blackPieces = screen.getAllByAltText('Black Piece');
     expect(redPieces).toHaveLength(12);
