8576def82c70bd1e079a25cdcb8d484e69be9000
Nicholas Persley <nsp73@nau.edu>
2023-11-19 01:23:06 -0700

    Add basic AI bot
    - Add functions that will go through possibilities on the game and calculate a move for the bot, this is not the best move
    - Create helper functions that help with this calculation

--- src/gameboard_dev/index.js
+++ src/gameboard_dev/index.js
@@ -6,53 +6,60 @@ const infoDisplay = document.querySelector("#info-display")
 
 let selectedSquare;
 let clickTarget;
+let max_search_depth = 4;
 
 const width = 8
+const height = 8
 
-const startPieces =
+const board =
     [
-    'empty', 'black', 'empty', 'black', 'empty', 'black', 'empty', 'black',
-    'black', 'empty', 'black', 'empty', 'black', 'empty', 'black', 'empty',
-    'empty', 'black', 'empty', 'black', 'empty', 'black', 'empty', 'black',
-    'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty',
-    'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty',
-    'red', 'empty', 'red', 'empty', 'red', 'empty', 'red', 'empty',
-    'empty', 'red', 'empty', 'red', 'empty', 'red', 'empty', 'red',
-    'red', 'empty', 'red', 'empty', 'red', 'empty', 'red', 'empty',
+    ['empty', 'black', 'empty', 'black', 'empty', 'black', 'empty', 'black'],
+    ['black', 'empty', 'black', 'empty', 'black', 'empty', 'black', 'empty'],
+    ['empty', 'black', 'empty', 'black', 'empty', 'black', 'empty', 'black'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ['red', 'empty', 'red', 'empty', 'red', 'empty', 'red', 'empty'],
+    ['empty', 'red', 'empty', 'red', 'empty', 'red', 'empty', 'red'],
+    ['red', 'empty', 'red', 'empty', 'red', 'empty', 'red', 'empty'],
     ]
 
 function createBoard() {
-  startPieces.forEach((piece, index) => {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.setAttribute('square-id', index);
+    let id = 0;
 
-    const rowIndex = Math.floor((63 - index) / 8) + 1;
-    const colIndex = index % 8;
+    board.forEach((row) => {
+        row.forEach((piece) => {
+          const square = document.createElement('div');
+          square.classList.add('square');
+          square.setAttribute('square-id', id);
 
-    if (rowIndex % 2 === 0) {
-      square.classList.add(colIndex % 2 === 0 ? "dark" : "light");
-    } else {
-      square.classList.add(colIndex % 2 === 0 ? "light" : "dark");
-    }
+          const rowIndex = Math.floor((63 - id) / 8) + 1;
+          const colIndex = id % 8;
 
-    if (piece === 'red') {
-      square.innerHTML = "<img draggable=true class='redPiece' src='redPiece.png'>";
-      square.firstChild?.classList.add('red');
-      square.firstChild?.setAttribute('draggable', true);
-    } else if (piece === 'black') {
-      square.innerHTML = "<img draggable=true class='blackPiece' src='blackPiece.png'>";
-      square.firstChild?.classList.add('black');
-      square.firstChild?.setAttribute('draggable', true);
-    }
+          if (rowIndex % 2 === 0) {
+            square.classList.add(colIndex % 2 === 0 ? "dark" : "light");
+          } else {
+            square.classList.add(colIndex % 2 === 0 ? "light" : "dark");
+          }
 
-    gameBoard.appendChild(square);
-  })
+          if (piece === 'red') {
+            square.innerHTML = "<img draggable=true class='redPiece' src='redPiece.png'>";
+            square.firstChild?.classList.add('red');
+            square.firstChild?.setAttribute('draggable', true);
+          } else if (piece === 'black') {
+            square.innerHTML = "<img draggable=true class='blackPiece' src='blackPiece.png'>";
+            square.firstChild?.classList.add('black');
+            square.firstChild?.setAttribute('draggable', true);
+          }
+
+          gameBoard.appendChild(square);
+          id += 1;
+        })
+    })
 }
 
 createBoard()
 
-function dragStart(event) {
+function dragEnd(event) {
   let squareId;
 
   if (event.target.classList.contains('square')) {
@@ -68,6 +75,8 @@ function dragStart(event) {
     if (canMove) {
         movePiece(squareId, destinationSquareId, event);
     }
+
+    triggerBot();
   }
 
 
@@ -137,9 +146,9 @@ function movePiece(squareId, destinationSquareId, event) {
     const { row: destRow, col: destCol } = getRowAndCol(destinationSquareId);
 
     // update the array representation of the board to reflect the move
-    const pieceToMove = startPieces[sourceRow * width + sourceCol];
-    startPieces[destRow * width + destCol] = pieceToMove;
-    startPieces[sourceRow * width + sourceCol] = 'empty';
+    const pieceToMove = board[sourceRow][sourceCol];
+    board[destRow][destCol] = pieceToMove;
+    board[sourceRow][sourceCol] = 'empty';
 
     // update the board on the screen
     renderBoard();
@@ -171,8 +180,173 @@ function makeListeners() {
     allSquares.forEach(square => {
         square.addEventListener('dragstart', setLastClick);
         square.addEventListener('dragover', dragOver);
-        square.addEventListener('drop', dragStart);
+        square.addEventListener('drop', dragEnd);
     });
 }
 
 makeListeners();
+
+// AI bot things
+function botThink()
+{
+    return botThinkHelper(board, 0, "black", null, null, null, null);
+}
+
+function botThinkHelper(currBoard, depth, colorTurn,
+                        currY, currX, nextY, nextX)
+{
+    var localBoard = structuredClone(currBoard);
+    var nextTurn = colorTurn.localeCompare("black") ? "red" : "black";
+
+    if(nextX != null && nextY != null)
+    {
+        botMovePieceInCalculation(localBoard, currX, currY,
+            nextX, nextY);
+    }
+
+    var startScore = getPositionScore(localBoard);
+    var bestMove = [startScore, currY, currX, nextY, nextX];
+
+    var piecePositions = getPiecePositions(colorTurn, currBoard);
+
+    if(depth <= max_search_depth)
+    {
+        piecePositions.forEach((pos) => {
+            var nextMoves = getPossibleMoves(localBoard, pos, colorTurn);
+
+            nextMoves.forEach((move) => {
+                var res = botThinkHelper(localBoard, depth + 1, nextTurn,
+                                         pos[0], pos[1], move[0], move[1]);
+
+                if(res[0] > bestMove[0])
+                {
+                    console.log(bestMove[0], pos, move);
+
+                    for(var index = 0; index < 5; index++)
+                    {
+                        bestMove[index] = res[index];
+                    }
+                }
+            })
+        });
+    }
+
+
+    return bestMove;
+}
+
+function getPositionScore(currBoard)
+{
+    var blackScore = 0, redScore = 0;
+
+    for(let row = 0; row < height; row++)
+    {
+        for(let col = 0; col < width; col++)
+        {
+            if(currBoard[row][col].localeCompare("red") == 0)
+            {
+                redScore++;
+            }
+            else if(currBoard[row][col].localeCompare("black") == 0)
+            {
+                blackScore++;
+            }
+        }
+    }
+
+    return blackScore - redScore;
+}
+
+function botMovePieceInCalculation(currBoard, fromY, fromX, toY, toX)
+{
+    currBoard[toY][toX] = currBoard[fromY][fromX];
+    currBoard[fromY][fromX] = "empty";
+
+    if(Math.abs(toY - fromY) != 1 && Math.abs(toX - fromX) != 1)
+    {
+        var middleY = parseInt((toY + fromY) / 2);
+        var middleX = parseInt((toX + fromX) / 2);
+
+        currBoard[middleY][middleX] = "empty";
+    }
+
+}
+
+function getPiecePositions(color, currBoard)
+{
+    var positions = [];
+
+    for(let row = 0; row < height; row++)
+    {
+        for(let col = 0; col < width; col++)
+        {
+            if(currBoard[row][col].localeCompare(color) == 0)
+            {
+                positions.push([row, col]);
+            }
+        }
+    }
+
+    return positions;
+}
+
+function getPossibleMoves(board, currPos, currColor)
+{
+    var yOffset = currColor.localeCompare("black") == 0 ? 1 : -1;
+    var possibleMoves = [];
+    var currY = currPos[0], currX = currPos[1];
+    var nextY = currY + yOffset;
+
+    for(var xOffset = -1; xOffset <= 1; xOffset += 2)
+    {
+        if(inBounds(nextY, currX + xOffset))
+        {
+            if(availableSpot(nextY, currX + xOffset, board))
+            {
+                possibleMoves.push([nextY, currX + xOffset]);
+            }
+            else if(canJump(nextY, currX + xOffset, board, currColor, yOffset, xOffset))
+            {
+                possibleMoves.push([nextY + yOffset, currX + (2 * xOffset)]);
+            }
+        }
+    }
+
+    return possibleMoves;
+}
+
+function inBounds(toY, toX)
+{
+    return toY < height && toY >= 0 && toX < width && toX >= 0;
+}
+
+function availableSpot(toY, toX, board)
+{
+    return inBounds(toY, toX) && board[toY][toX].localeCompare("empty") == 0;
+}
+
+function canJump(toY, toX, board, color, yOffset, xOffset)
+{
+    if(!isOppositeColor(board, color, toY, toX) ||
+       !availableSpot(toY + yOffset, toX + xOffset, board))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+function isOppositeColor(board, color, currY, currX)
+{
+    return board[currY][currX].localeCompare(color) != 0 &&
+           board[currY][currX].localeCompare("empty") != 0;
+}
+
+function triggerBot()
+{
+    var res = botThink();
+    console.log(res);
+    botMovePieceInCalculation(board, res[1], res[2], res[3], res[4]);
+    console.log(board);
+    renderBoard();
+}
