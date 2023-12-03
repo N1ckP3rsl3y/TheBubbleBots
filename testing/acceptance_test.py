a11d9bc7bc059a140f2905a70c5d8b1f3b2192c8
tjc234 <tjc234@nau.edu>
2023-12-03 11:04:42 -0700

    add testing for about page in acceptance test

--- testing/acceptance_test.py
+++ testing/acceptance_test.py
@@ -6,33 +6,48 @@ import pytest
 
 @pytest.fixture
 def browser():
-    # Create Chrome options
+    # create Chrome options
     options = Options()
 
-    # Add arguments for headless mode and disable GPU
+    # add arguments for headless mode and disable GPU
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920x1080")  # Set the window size
     options.add_argument("--disable-notifications")
 
-    # Create a web driver instance with the specified options
+    # create a web driver instance with the specified options
     driver = webdriver.Chrome(options=options)
     yield driver
 
-    # Close the browser
+    # close the browser
     driver.quit()
 
 def test_play_now_button(browser):
-    # Open the landing page
+    # open the landing page
     browser.get("http://www.bubblebots.xyz:1034/")
 
-    # Find and click the "Play Now" button on the home page
+    # find and click the "Play Now" button on the home page
     play_now_button = browser.find_element(By.CLASS_NAME, "button")
     play_now_button.click()
 
-    # Give some time for the page transition
+    # give some time for the page transition
     time.sleep(5)
 
-    # Verify that we are on the game page
+    # verify that we are on the game page
     game_board = browser.find_element(By.ID, "gameboard")
     assert game_board.is_displayed(), "Failed to reach the game page."
+
+def test_about_button(browser):
+    # open the landing page
+    browser.get("http://www.bubblebots.xyz:1034/")
+
+    # find and click the "About" button on the home page
+    about_button = browser.find_element(By.LINK_TEXT, "About")
+    about_button.click()
+
+    # give some time for the page transition
+    time.sleep(5)
+
+    # verify that we are on the about page
+    about_heading = browser.find_element(By.TAG_NAME, "h1")
+    assert about_heading.text == "About BubbleBots", "Failed to reach the About page."
