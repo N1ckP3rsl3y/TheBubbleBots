abc98442eaf28da802b280f189b84f7fd66e22a5
tjc234 <tjc234@nau.edu>
2023-12-03 19:47:19 -0700

    change to slider test

--- testing/__tests__/board.test.js
+++ testing/__tests__/board.test.js
@@ -1,66 +1,47 @@
-// board.test.js
-
+// Import necessary functions and modules
 const {
     createBoard,
     renderBoard,
     makeListeners,
-    isValidMove,
-    movePiece,
-    getRowAndCol,
-    botThinkHelper,
-    getPositionScore,
-    botMovePieceInCalculation,
-    getPiecePositions,
-    getPossibleMoves,
-    availableSpot,
-    canJump,
-    isOppositeColor,
   } = require('../../src/board.js');
-
   
-  describe('Checkers Board', () => {
-    // Mock data for testing
-    const mockBoard = [
-        ['empty', 'black', 'empty', 'black', 'empty', 'black', 'empty', 'black'],
-        ['black', 'empty', 'black', 'empty', 'black', 'empty', 'black', 'empty'],
-        ['empty', 'black', 'empty', 'black', 'empty', 'black', 'empty', 'black'],
-        ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-        ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-        ['red', 'empty', 'red', 'empty', 'red', 'empty', 'red', 'empty'],
-        ['empty', 'red', 'empty', 'red', 'empty', 'red', 'empty', 'red'],
-        ['red', 'empty', 'red', 'empty', 'red', 'empty', 'red', 'empty']
-    ];
-    
+  // Import testing library
+  const { fireEvent } = require('@testing-library/dom');
   
-    beforeEach(() => {
-      // Set up a fresh board before each test
-      document.body.innerHTML = '<div id="gameboard"></div>';
-      createBoard();
+  describe('Sliders', () => {
+    // Mock data or setup as needed for the slider tests
+  
+    // Test slider functionality
+    test('Slider functionality', () => {
+      // Mock the HTML structure of your slider
+      document.body.innerHTML = `
+        <div class="range-slider">
+          <input type="range" class="slider" max="100" />
+          <div class="slider-thumb"></div>
+          <div class="tooltip"></div>
+          <div class="progress"></div>
+        </div>
+      `;
+  
+      // Get the slider element
+      const slider = document.querySelector('.slider');
+  
+      // Trigger a change in the slider value
+      fireEvent.input(slider, { target: { value: 50 } });
+  
+      // Now, you can assert the expected behavior based on the slider value
+      // For example, expect certain UI changes or side effects:
+      // For instance, you might expect the tooltip to have the updated value:
+      const tooltip = document.querySelector('.tooltip');
+      expect(tooltip.textContent).toBe('50');
     });
   
-    test('Board creation', () => {
-      // Verify that the board is created correctly
-      const gameboard = document.getElementById('gameboard');
-      expect(gameboard).toBeTruthy();
+    // Add more slider tests as needed...
+  
+    // Clean up after each test
+    afterEach(() => {
+      // Optionally clean up the DOM or reset any state as needed
+      document.body.innerHTML = '';
     });
-  
-    test('Piece movement', () => {
-      // Mock a drag-and-drop event
-      const dragStartEvent = new Event('dragstart');
-      const dragEndEvent = new Event('drop');
-      const square1 = document.querySelector('.square[square-id="0"]');
-      const square2 = document.querySelector('.square[square-id="9"]');
-      square1.dispatchEvent(dragStartEvent);
-      square2.dispatchEvent(dragEndEvent);
-  
-      // Validate that the piece has moved
-      const pieceInSquare1 = document.querySelector('.square[square-id="0"] img');
-      const pieceInSquare2 = document.querySelector('.square[square-id="9"] img');
-      expect(pieceInSquare1).toBeFalsy();
-      expect(pieceInSquare2).toBeTruthy();
-    });
-  
   });
-
-  
   
